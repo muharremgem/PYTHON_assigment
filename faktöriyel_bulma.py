1178462bb9553bfbb65de2b56fd2fa31d5dcfe5c
Muharrem GEM <104659843+muharremgem@users.noreply.github.com>
2022-06-23 15:21:48 +0300

    Add files via upload

---
+++ faktöriyel_bulma.py
@@ -0,0 +1,43 @@
+print("""
+**************
+Faktöriyel Bulma Programı
+
+Çıkmak için q'ya basın.
+***************
+""")
+
+while True:
+    sayı = input("Sayı:")
+    if (sayı =="q"):
+        print("Program Sonlandırılıyor.")
+        break
+
+    else :
+        sayı = int(sayı)
+
+        faktoriyel = 1
+
+        for i in range(2,sayı+1):
+            print("Faktoriyel",faktoriyel, "i:",i)
+            faktoriyel *=i
+        print("Faktoriyel", faktoriyel)    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
