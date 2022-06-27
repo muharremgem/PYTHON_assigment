7a5403b20dfd5a6e3409b05be03f68f36ab1a907
Muharrem GEM <104659843+muharremgem@users.noreply.github.com>
2022-06-27 13:20:10 +0300

    Add files via upload

---
+++ (sayılarıntoplamı_değş_atama).py
@@ -0,0 +1,17 @@
+"""Her bir while döngüsünde kullanıcıdan bir sayı alın ve kullanıcının girdiği sayıları "toplam" isimli bir değişkene ekleyin. Kullanıcı "q" tuşuna bastığı zaman döngüyü sonlandırın ve ekrana "toplam değişkenini" bastırın.
+
+İpucu : while döngüsünü sonsuz koşulla başlatın ve kullanıcı q'ya basarsa döngüyü break ile sonlandırın."""
+
+toplam = 0
+
+while True:
+
+    sayı = input("sayı:")
+
+    if (sayı == "q"):
+        break
+    sayı = int(sayı)
+
+    toplam += sayı
+
+print("Girdiğiniz Sayıların Toplmaı:",toplam)
