f947b3cb1e137d4541ea91e02ba452ad73362159
Muharrem GEM <104659843+muharremgem@users.noreply.github.com>
2022-07-03 19:53:59 +0300

    Add files via upload

---
+++ (ebob).py
@@ -0,0 +1,22 @@
+"""Kullanıcıdan 2 tane sayı alarak bu sayıların en büyük ortak bölenini (EBOB) dönen bir tane fonksiyon yazın."""
+
+
+def ebob_bulma(sayı1,sayı2):
+
+    i = 1
+    ebob = 1
+    while (i <= sayı1 and i <= sayı2 ):
+
+        if( not (sayı1 % i) and not (sayı2 % i)):
+            ebob = i
+        i += 1 
+    return ebob
+
+sayı1 = int(input("sayı-1:"))           
+sayı2 = int(input("sayı-2:"))
+
+print("ebob:",ebob_bulma(sayı1,sayı2))
+
+
+
+
