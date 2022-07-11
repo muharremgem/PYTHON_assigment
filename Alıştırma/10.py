1451066876a1bb6f9e5a039c6793f3af9b449d41
Muharrem GEM <104659843+muharremgem@users.noreply.github.com>
2022-07-11 09:04:53 +0300

    Add files via upload

---
+++ Alıştırma/10.py
@@ -0,0 +1,6 @@
+# 0 ile 100 arasındaki çift olan sayıların toplamını yazdıran kodları for döngüsü ile yapalım.
+toplam=0
+for i in range(0,100,2):
+  toplam +=i
+print(toplam)
+
---
+++ Alıştırma/7.py
@@ -0,0 +1,8 @@
+#1-100 arası Çift Sayıları Listeleyen Python kodlarını yazalım.
+
+
+for x in list(range(1,101)):
+  if x%2 == 0:
+    print(x)
+
+
